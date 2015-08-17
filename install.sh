d0dfc87f77c0df031ac6906235c5d2c7034c3bf9
Alan Bradley <alan@gangleri.net>
2015-08-17 12:00:06 +0100

    installer will check the md5sum of the installed exec

--- install.sh
+++ install.sh
@@ -4,14 +4,17 @@ set -e
 
 { # ensure the entire script is downloaded
 
-	VERSION='1.0.0'
-	EXE_URL='https://github.com/gangleri/nextport/releases/download/${VERSION}/nextport'
-	MD5_URL='https://github.com/gangleri/nextport/releases/download/${VERSION}/md5sum'
+	EXE_URL='https://github.com/gangleri/nextport/releases/download/1.0.0/nextport'
+	MD5_URL='https://github.com/gangleri/nextport/releases/download/1.0.0/md5sum'
 	INSTALL_NAME='/usr/local/bin/nextport'
 	MD5SUM='/usr/local/bin/md5sum'
 	
 	curl  -s -L -o $INSTALL_NAME $EXE_URL
-	curl  -s -L -o $INSTALL_NAME $MD5_URL
+	curl  -s -L -o $MD5SUM $MD5_URL
 	chmod +x $INSTALL_NAME
+	pushd `pwd` > /dev/null
+	cd '/usr/local/bin'
 	md5sum -c $MD5SUM
+	rm -f $MD5SUM > /dev/null
+	popd > /dev/null
 }
